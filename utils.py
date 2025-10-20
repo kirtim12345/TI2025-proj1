c697f43495255f7f9111a87f7798d5d232dbbb06
Pedro Guerreiro <pmguerre@ualg.pt>
2025-10-20 14:59:25 +0100

    Adicionei uma função para ler strings

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ def get_int(string):
     i = input(string)
     return int(i)
 
+
 def get_float(msg):
     f = float(input(msg))
     return f
+
+
+def get_string(msg):
+    s = input(msg)
+    return s
